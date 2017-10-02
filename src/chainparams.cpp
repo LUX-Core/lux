5ce683a67fefad25c5855dc0d605c8262784660c
216k155 <shacker@i2pd.net>
2017-10-03 04:34:19 +1100

    Initilized chainparams & chainparamsseeds

---
+++ src/chainparams.cpp
@@ -0,0 +1,227 @@
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Copyright (c) 2017 The BHCoin developers
+
+#include "assert.h"
+
+#include "chainparams.h"
+#include "main.h"
+#include "util.h"
+
+#include <boost/assign/list_of.hpp>
+
+using namespace boost::assign;
+
+struct SeedSpec6 {
+    uint8_t addr[16];
+    uint16_t port;
+};
+
+#include "chainparamsseeds.h"
+
+// Mainnet
+// Need some more configure to generate new genesis block. Note: we hide our genesis block & hashMerkleRoot for later start. 
+// Convert the pnSeeds6 array into usable address objects.
+static void convertSeed6(std::vector<CAddress> &vSeedsOut, const SeedSpec6 *data, unsigned int count)
+{
+    // It'll only connect to one or two seed nodes because once it connects,
+    // it'll get a pile of addresses with newer timestamps.
+
+    const int64_t nOneWeek = 7*24*60*60;
+    for (unsigned int i = 0; i < count; i++)
+    {
+        struct in6_addr ip;
+        memcpy(&ip, data[i].addr, sizeof(ip));
+        CAddress addr(CService(ip, data[i].port));
+        addr.nTime = GetTime() - GetRand(nOneWeek) - nOneWeek;
+        vSeedsOut.push_back(addr);
+    }
+}
+
+class CMainParams : public CChainParams {
+public:
+    CMainParams() {
+        // The message start string is designed to be unlikely to occur in normal data.
+        // The characters are rarely used upper ASCII, not valid as UTF-8, and produce
+        // a large 4-byte int at any alignment.
+        pchMessageStart[0] = 0xdf;
+        pchMessageStart[1] = 0xe2;
+        pchMessageStart[2] = 0xa2;
+        pchMessageStart[3] = 0xcd;
+        vAlertPubKey = ParseHex("0486phi16120d543f104cbff2bd23680056a3b9ea05e1137d2ff90eeb5e08472eb500322593a2cb06fbf8297d7beb6cd30cb90f98153b5b7cce1493749e41e0284");
+        nDefaultPort = 6666;
+        nRPCPort = 6667;
+        bnProofOfWorkLimit = CBigNum(~uint256(0) >> 20);
+
+        // Build the genesis block. Note that the output of the genesis coinbase cannot
+        // be spent as it did not originally exist in the database.
+
+        const char* pszTimestamp = "THE NEW IMPLEMENTATION OF PHI1612 HASH"; // Input random pszTimestamp to generate new genesis block
+        std::vector<CTxIn> vin;
+        vin.resize(1);
+        vin[0].scriptSig = CScript() << 0 << CBigNum(42) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
+        std::vector<CTxOut> vout;
+        vout.resize(1);
+        vout[0].SetEmpty();
+        CTransaction txNew(1, 1506923060, vin, vout, 0);
+        genesis.vtx.push_back(txNew);
+        genesis.hashPrevBlock = 0;
+        genesis.hashMerkleRoot = genesis.BuildMerkleTree();
+        genesis.nVersion = 1;
+        genesis.nTime    = 1506923060; // Replace epochtime to generate new genesis block
+        genesis.nBits    = bnProofOfWorkLimit.GetCompact();
+        genesis.nNonce   = 0; // Input nNonce 
+        
+        // Comment assert to generate genesis block and hash merkle root
+        // Get hashMerkleRoot by using ./bhcoind getblock {Genesis-Block} 
+        hashGenesisBlock = genesis.GetHash();
+        assert(hashGenesisBlock == uint256("")); // Add Genesisblock 
+        assert(genesis.hashMerkleRoot == uint256("")); // Add hashMerkleRoot
+
+        //MineGenesis(genesis);
+
+        vSeeds.push_back(CDNSSeedData("", "")); // Add seednode for later start
+
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 25); // BHCoin address started with B
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 125);
+        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1, (63+128));
+        base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x03)(0x82)(0xC8)(0x6E).convert_to_container<std::vector<unsigned char> >();
+        base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x03)(0x82)(0xB6)(0x5D).convert_to_container<std::vector<unsigned char> >();
+
+        convertSeed6(vFixedSeeds, pnSeed6_main, ARRAYLEN(pnSeed6_main));
+
+        nLastPOWBlock = 1000000;
+        nPOSStartBlock = 10000;
+    }
+
+    virtual const CBlock& GenesisBlock() const { return genesis; }
+    virtual Network NetworkID() const { return CChainParams::MAIN; }
+
+    virtual const vector<CAddress>& FixedSeeds() const {
+        return vFixedSeeds;
+    }
+protected:
+    CBlock genesis;
+    vector<CAddress> vFixedSeeds;
+};
+static CMainParams mainParams;
+
+
+
+// Testnet
+// Dont need to modifiled anything under this line
+//------------------------------------------------//
+class CTestNetParams : public CMainParams {
+public:
+    CTestNetParams() {
+      
+        pchMessageStart[0] = 0x62;
+        pchMessageStart[1] = 0x44;
+        pchMessageStart[2] = 0x85;
+        pchMessageStart[3] = 0x11;
+
+        bnProofOfWorkLimit = CBigNum(~uint256(0) >> 16);
+        vAlertPubKey = ParseHex("0471sc261db490094d35cde15b1f5d755fa6ad6f2b5ed0f340e3f17f57389c3c2af113a8cbcc885bde73305a553b5640c83021128008ddf882e856336269080496");
+        nDefaultPort = 26178;
+        nRPCPort = 26174;
+
+        strDataDir = "testnet";
+        // Modify the testnet genesis block so the timestamp is valid for a later start.
+        genesis.nBits  = bnProofOfWorkLimit.GetCompact();
+        genesis.nNonce = 0; // Input nNonce after generated genesis block for testnet used
+        genesis.nTime    = 1506923060;
+  
+        //hashGenesisBlock = genesis.GetHash();
+         
+        //assert(hashGenesisBlock == uint256("0x"));
+
+        vFixedSeeds.clear();
+        vSeeds.clear();
+        vSeeds.push_back(CDNSSeedData("", "")); // Add seednode for later start
+
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 62); 
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 196);
+        base58Prefixes[SECRET_KEY]     = std::vector<unsigned char>(1, 65 + 128);
+        base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x35)(0x87)(0xCF).convert_to_container<std::vector<unsigned char> >();
+        base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x35)(0x83)(0x94).convert_to_container<std::vector<unsigned char> >();
+
+        convertSeed6(vFixedSeeds, pnSeed6_test, ARRAYLEN(pnSeed6_test));
+
+        nLastPOWBlock = 0x7fffffff;
+    }
+    virtual Network NetworkID() const { return CChainParams::TESTNET; }
+};
+static CTestNetParams testNetParams;
+
+
+
+// Regression test
+// Dont need to modified anything under this line
+//------------------------------------------------//
+class CRegTestParams : public CTestNetParams {
+public:
+    CRegTestParams() {
+        pchMessageStart[0] = 0xf2;
+        pchMessageStart[1] = 0xbe;
+        pchMessageStart[2] = 0xb8;
+        pchMessageStart[3] = 0xd1;
+        bnProofOfWorkLimit = CBigNum(~uint256(0) >> 1);
+        genesis.nTime = 1506923060;
+        genesis.nBits  = bnProofOfWorkLimit.GetCompact();
+        genesis.nNonce = 1815802; // Random nNonce for testing purposed
+        hashGenesisBlock = genesis.GetHash();
+        nDefaultPort = 18444;
+        strDataDir = "regtest";
+
+        // Comment asser to generate genesis block for regtest
+        //MineGenesis(genesis);
+        //assert(hashGenesisBlock == uint256(""));
+
+        vSeeds.clear();  // Regtest mode doesn't have any DNS seeds.
+    }
+
+    virtual bool RequireRPCPassword() const { return false; }
+    virtual Network NetworkID() const { return CChainParams::REGTEST; }
+};
+static CRegTestParams regTestParams;
+
+static CChainParams *pCurrentParams = &mainParams;
+
+const CChainParams &Params() {
+    return *pCurrentParams;
+}
+
+void SelectParams(CChainParams::Network network) {
+    switch (network) {
+        case CChainParams::MAIN:
+            pCurrentParams = &mainParams;
+            break;
+        case CChainParams::TESTNET:
+            pCurrentParams = &testNetParams;
+            break;
+        case CChainParams::REGTEST:
+            pCurrentParams = &regTestParams;
+            break;
+        default:
+            assert(false && "Unimplemented network");
+            return;
+    }
+}
+
+bool SelectParamsFromCommandLine() {
+    bool fRegTest = GetBoolArg("-regtest", false);
+    bool fTestNet = GetBoolArg("-testnet", false);
+
+    if (fTestNet && fRegTest) {
+        return false;
+    }
+
+    if (fRegTest) {
+        SelectParams(CChainParams::REGTEST);
+    } else if (fTestNet) {
+        SelectParams(CChainParams::TESTNET);
+    } else {
+        SelectParams(CChainParams::MAIN);
+    }
+    return true;
+}
