17ea41cf5dff3cfe3c3f45ce13151db455fd23b6
216k155 <32459326+216k155@users.noreply.github.com>
2017-11-09 02:39:42 +1100

    Update version.h

--- src/version.h
+++ src/version.h
@@ -36,11 +36,11 @@ static const int PROTOCOL_VERSION = 69200;
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 69111;
+static const int MIN_PEER_PROTO_VERSION = 69110;
 
-static const int MIN_INSTANTX_PROTO_VERSION = 69111;
+static const int MIN_INSTANTX_PROTO_VERSION = 69110;
 
-static const int MIN_MN_PROTO_VERSION = 69111;
+static const int MIN_MN_PROTO_VERSION = 69110;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
@@ -48,7 +48,7 @@ static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 69000;
-static const int NOBLKS_VERSION_END = 69110;
+static const int NOBLKS_VERSION_END = 69010;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
