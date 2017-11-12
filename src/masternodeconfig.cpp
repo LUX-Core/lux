02ca43ad4abf7b3adad38b74788470aa5ec7537d
216k155 <216k155@luxcore.io>
2017-11-12 12:17:59 -0800

    Added masternode-pos (masternodeconfig.cpp/h)(upgrade to dash v0.11.2)

--- src/masternodeconfig.cpp
+++ src/masternodeconfig.cpp
@@ -2,11 +2,12 @@
 #include "net.h"
 #include "masternodeconfig.h"
 #include "util.h"
+#include <base58.h>
 
 CMasternodeConfig masternodeConfig;
 
-void CMasternodeConfig::add(std::string alias, std::string ip, std::string privKey, std::string txHash, std::string outputIndex) {
-    CMasternodeEntry cme(alias, ip, privKey, txHash, outputIndex);
+void CMasternodeConfig::add(std::string alias, std::string ip, std::string privKey, std::string txHash, std::string outputIndex, std::string donationAddress, std::string donationPercent) {
+    CMasternodeEntry cme(alias, ip, privKey, txHash, outputIndex, donationAddress, donationPercent);
     entries.push_back(cme);
 }
 
@@ -22,31 +23,51 @@ bool CMasternodeConfig::read(std::string& strErr) {
             continue;
         }
         std::istringstream iss(line);
-        std::string alias, ip, privKey, txHash, outputIndex;
-        if (!(iss >> alias >> ip >> privKey >> txHash >> outputIndex)) {
-            strErr = "Could not parse masternode.conf line: " + line;
-            streamConfig.close();
-            return false;
-        }
-
-        if(Params().NetworkID() == CChainParams::MAIN){
-            if(CService(ip).GetPort() != 17170) {
-                LogPrintf("Invalid port detected in masternode.conf: %s (must be 17170 for mainnet)\n", line.c_str());
+        std::string alias, ip, privKey, txHash, outputIndex, donation, donationAddress, donationPercent;
+        if (!(iss >> alias >> ip >> privKey >> txHash >> outputIndex >> donation)) {
+            donationAddress = "";
+            donationPercent = "";
+            iss.str(line);
+            iss.clear();
+            if (!(iss >> alias >> ip >> privKey >> txHash >> outputIndex)) {
+                strErr = "Could not parse masternode.conf line: " + line;
                 streamConfig.close();
                 return false;
             }
-        } else if(CService(ip).GetPort() == 17170) {
-            LogPrintf("Invalid port detected in masternode.conf: %s (17170 must be only on mainnet)\n", line.c_str());
+        } else {
+            size_t pos = donation.find_first_of(":");
+            if(pos == string::npos) { // no ":" found
+                donationPercent = "100";
+                donationAddress = donation;
+            } else {
+                donationPercent = donation.substr(pos + 1);
+                donationAddress = donation.substr(0, pos);
+            }
+            CBitcoinAddress address(donationAddress);
+            if (!address.IsValid()) {
+                strErr = "Invalid Dash address in masternode.conf line: " + line;
+                streamConfig.close();
+                return false;
+            }
+        }
+
+        if(Params().NetworkID() == CChainParams::MAIN){
+            if(CService(ip).GetPort() != 28666) {
+                strErr = "Invalid port detected in masternode.conf: " + line + " (must be 9999 for mainnet)";
+                streamConfig.close();
+                return false;
+            }
+        } else if(CService(ip).GetPort() == 28666) {
+            strErr = "Invalid port detected in masternode.conf: " + line + " (9999 must be only on mainnet)";
             streamConfig.close();
             return false;
         }
 
-        if (!(CService(ip).IsIPv4() && CService(ip).IsRoutable())) {
-            LogPrintf("Invalid Address detected in masternode.conf: %s (IPV4 ONLY) \n", line.c_str());
-            streamConfig.close();
-            return false;
-        }
 
-        add(alias, ip, privKey, txHash, outputIndex);
+        add(alias, ip, privKey, txHash, outputIndex, donationAddress, donationPercent);
     }
+
+    streamConfig.close();
+    return true;
 }
+
