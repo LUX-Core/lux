05899068274b70ed7ec162adb8a124e71f0e9791
Duzy Chan <code@duzy.info>
2018-01-07 21:30:44 +0800

    Code cleanup.

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -297,14 +297,12 @@ bool CheckStakeKernelHash(unsigned int nBits, const CBlock &blockFrom, const CTr
 
     std::cout << "bnTarget: " << bnTarget.GetHex() << std::endl;
 
-
     // Weighted target
     int64_t nValueIn = txPrev.vout[prevout.n].nValue;
     uint256 bnWeight = uint256(nValueIn);
     bnTarget *= bnWeight;
 
     std::cout << "targetProofOfStake: " << bnTarget.GetHex() << std::endl;
-
 
     uint64_t nStakeModifier = pindexPrev->nStakeModifier;
     int nStakeModifierHeight = pindexPrev->nHeight;
@@ -318,7 +316,6 @@ bool CheckStakeKernelHash(unsigned int nBits, const CBlock &blockFrom, const CTr
     std::cout << "nStakeModifierHeight: " << nStakeModifierHeight << std::endl;
     std::cout << "nStakeModifierTime: " << nStakeModifierTime << std::endl;
 
-
     // Calculate hash
     CDataStream ss(SER_GETHASH, 0);
     unsigned int nTimeBlockFrom = blockFrom.GetBlockTime();
@@ -326,7 +323,6 @@ bool CheckStakeKernelHash(unsigned int nBits, const CBlock &blockFrom, const CTr
     hashProofOfStake = Hash(ss.begin(), ss.end());
 
     std::cout << "hashProofOfStake: " << hashProofOfStake.GetHex() << std::endl;
-
 
     if (fPrintProofOfStake)
     {
@@ -357,143 +353,7 @@ bool CheckStakeKernelHash(unsigned int nBits, const CBlock &blockFrom, const CTr
     }
 
     return true;
-
-
-
-
-
-//    //assign new variables to make it easier to read
-//    int64_t nValueIn = txPrev.vout[prevout.n].nValue;
-//    unsigned int nTimeBlockFrom = blockFrom.GetBlockTime();
-//
-//    if (nTimeTx < nTimeBlockFrom) // Transaction timestamp violation
-//        return error("CheckStakeKernelHash() : nTime violation");
-//
-//    if (nTimeBlockFrom + nStakeMinAge > nTimeTx) // Min age requirement
-//        return error("CheckStakeKernelHash() : min age violation - nTimeBlockFrom=%d nStakeMinAge=%d nTimeTx=%d", nTimeBlockFrom, nStakeMinAge, nTimeTx);
-//
-//    //grab difficulty
-//    uint256 bnTargetPerCoinDay;
-//    bnTargetPerCoinDay.SetCompact(nBits);
-//
-//    //grab stake modifier
-//    uint64_t nStakeModifier = 0;
-//    int nStakeModifierHeight = 0;
-//    int64_t nStakeModifierTime = 0;
-//    if (!GetKernelStakeModifier(blockFrom.GetHash(), nStakeModifier, nStakeModifierHeight, nStakeModifierTime, fPrintProofOfStake)) {
-//        LogPrintf("CheckStakeKernelHash(): failed to get kernel stake modifier \n");
-//        return false;
-//    }
-//
-//    //create data stream once instead of repeating it in the loop
-//    CDataStream ss(SER_GETHASH, 0);
-//    ss << nStakeModifier;
-//
-//    //if wallet is simply checking to make sure a hash is valid
-//    if (fCheck) {
-//        hashProofOfStake = stakeHash(nTimeTx, ss, prevout.n, prevout.hash, nTimeBlockFrom, txPrev.nTime);
-//        return stakeTargetHit(hashProofOfStake, nValueIn, bnTargetPerCoinDay);
-//    }
-//
-//    bool fSuccess = false;
-//    unsigned int nTryTime = 0;
-//    unsigned int i;
-//    for (i = 0; i < (nHashDrift); i++) //iterate the hashing
-//    {
-//        //hash this iteration
-//        nTryTime = nTimeTx + nHashDrift - i;
-//        hashProofOfStake = stakeHash(nTryTime, ss, prevout.n, prevout.hash, nTimeBlockFrom, txPrev.nTime);
-//
-//        // if stake hash does not meet the target then continue to next iteration
-//        if (!stakeTargetHit(hashProofOfStake, nValueIn, bnTargetPerCoinDay))
-//            continue;
-//
-//        fSuccess = true; // if we make it this far then we have successfully created a stake hash
-//        nTimeTx = nTryTime;
-//
-//        if (fDebug || fPrintProofOfStake) {
-//            LogPrintf("CheckStakeKernelHash() : using modifier %s at height=%d timestamp=%s for block from height=%d timestamp=%s\n",
-//                boost::lexical_cast<std::string>(nStakeModifier).c_str(), nStakeModifierHeight,
-//                DateTimeStrFormat("%Y-%m-%d %H:%M:%S", nStakeModifierTime).c_str(),
-//                mapBlockIndex[blockFrom.GetHash()]->nHeight,
-//                DateTimeStrFormat("%Y-%m-%d %H:%M:%S", blockFrom.GetBlockTime()).c_str());
-//            LogPrintf("CheckStakeKernelHash() : pass protocol=%s modifier=%s nTimeBlockFrom=%u prevoutHash=%s nTimeTxPrev=%u nPrevout=%u nTimeTx=%u hashProof=%s\n",
-//                "0.3",
-//                boost::lexical_cast<std::string>(nStakeModifier).c_str(),
-//                nTimeBlockFrom, prevout.hash.ToString().c_str(), nTimeBlockFrom, prevout.n, nTryTime,
-//                hashProofOfStake.ToString().c_str());
-//        }
-//        break;
-//    }
-//
-//    mapHashedBlocks.clear();
-//    mapHashedBlocks[chainActive.Tip()->nHeight] = GetTime(); //store a time stamp of when we last hashed on this block
-//    return fSuccess;
 }
-
-//bool CheckStakeKernelHashV1(unsigned int nBits, const CBlock blockFrom, const CTransaction txPrev, const COutPoint prevout, unsigned int& nTimeTx, unsigned int nHashDrift, bool fCheck, uint256& hashProofOfStake, bool fPrintProofOfStake) {
-//    if (nTimeTx < txPrev.nTime)  // Transaction timestamp violation
-//        return error("CheckStakeKernelHash() : nTime violation");
-//
-//    unsigned int nTimeBlockFrom = blockFrom.GetBlockTime();
-//    if (nTimeBlockFrom + nStakeMinAge > nTimeTx) // Min age requirement
-//        return error("CheckStakeKernelHash() : min age violation");
-//
-//    uint256 bnTargetPerCoinDay;
-//    bnTargetPerCoinDay.SetCompact(nBits);
-//    int64_t nValueIn = txPrev.vout[prevout.n].nValue;
-//
-//    uint256 hashBlockFrom = blockFrom.GetHash();
-//
-//    uint256 bnCoinDayWeight = uint256(nValueIn) * GetWeight((int64_t)txPrev.nTime, (int64_t)nTimeTx) / COIN / (24 * 60 * 60);
-//    targetProofOfStake = (bnCoinDayWeight * bnTargetPerCoinDay).getuint256();
-//
-//    // Calculate hash
-//    CDataStream ss(SER_GETHASH, 0);
-//    uint64_t nStakeModifier = 0;
-//    int nStakeModifierHeight = 0;
-//    int64_t nStakeModifierTime = 0;
-//
-//    if (!GetKernelStakeModifier(hashBlockFrom, nStakeModifier, nStakeModifierHeight, nStakeModifierTime, fPrintProofOfStake))
-//        return false;
-//    ss << nStakeModifier;
-//
-//    ss << nTimeBlockFrom << nTxPrevOffset << txPrev.nTime << prevout.n << nTimeTx;
-//    hashProofOfStake = Hash(ss.begin(), ss.end());
-//    if (fPrintProofOfStake)
-//    {
-//        LogPrintf("CheckStakeKernelHash() : using modifier 0x%016x at height=%d timestamp=%s for block from height=%d timestamp=%s\n",
-//                  nStakeModifier, nStakeModifierHeight,
-//                  DateTimeStrFormat("%Y-%m-%d %H:%M:%S", nStakeModifierTime).c_str(),
-//                  mapBlockIndex[hashBlockFrom]->nHeight,
-//                  DateTimeStrFormat("%Y-%m-%d %H:%M:%S", blockFrom.GetBlockTime()).c_str());
-//        LogPrintf("CheckStakeKernelHash() : check modifier=0x%016x nTimeBlockFrom=%u nTxPrevOffset=%u nTimeTxPrev=%u nPrevout=%u nTimeTx=%u hashProof=%s\n",
-//                  nStakeModifier,
-//                  nTimeBlockFrom, nTxPrevOffset, txPrev.nTime, prevout.n, nTimeTx,
-//                  hashProofOfStake.ToString());
-//    }
-//
-//    // Now check if proof-of-stake hash meets target protocol
-//    if (uint256(hashProofOfStake) > bnCoinDayWeight * bnTargetPerCoinDay)
-//        return false;
-//    if (fDebug && !fPrintProofOfStake)
-//    {
-//        LogPrintf("CheckStakeKernelHash() : using modifier 0x%016x at height=%d timestamp=%s for block from height=%d timestamp=%s\n",
-//                  nStakeModifier, nStakeModifierHeight,
-//                  DateTimeStrFormat("%Y-%m-%d %H:%M:%S", nStakeModifierTime).c_str(),
-//                  mapBlockIndex[hashBlockFrom]->nHeight,
-//                  DateTimeStrFormat("%Y-%m-%d %H:%M:%S", blockFrom.GetBlockTime()).c_str());
-//        LogPrintf("CheckStakeKernelHash() : pass modifier=0x%016x nTimeBlockFrom=%u nTxPrevOffset=%u nTimeTxPrev=%u nPrevout=%u nTimeTx=%u hashProof=%s\n",
-//                  nStakeModifier,
-//                  nTimeBlockFrom, nTxPrevOffset, txPrev.nTime, prevout.n, nTimeTx,
-//                  hashProofOfStake.ToString());
-//    }
-//    return true;
-//}
-
-//bool CheckStakeKernelHashV2(unsigned int nBits, const CBlock blockFrom, const CTransaction txPrev, const COutPoint prevout, unsigned int& nTimeTx, unsigned int nHashDrift, bool fCheck, uint256& hashProofOfStake, bool fPrintProofOfStake) {
-//
-//}
 
 // Check kernel hash target and coinstake signature
 bool CheckProofOfStake(const CBlock &block, uint256& hashProofOfStake)
