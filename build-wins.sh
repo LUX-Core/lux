91b5bb4df4d7f839988229fdddd0dfaa70d6225c
216k155 <32459326+216k155@users.noreply.github.com>
2018-01-07 22:11:11 +1100

    Update build-wins.sh

--- build-wins.sh
+++ build-wins.sh
@@ -44,34 +44,35 @@ clonerepo() { #TODO: add error detection
 	message "Cloning LUX source from master repository..."
   	cd ~/
 	git clone https://github.com/216k155/lux.git
+	sudo chmod -R a+rw lux
 }
 
 compile() {
 	cd lux #TODO: squash relative path
-	message "Preparing HOST mingw32 to build ...this may take a few minutes..."
-	cd depends && make HOST=x86_64-w64-mingw32
-	if [ $? -ne 0 ]; then error; fi
-    message "Install libdb4.8 (Berkeley DB4)"
-	cd ..
-    if [ $? -ne 0 ]; then error; fi
-	message "Build LUX...this may take a few minutes..."
-	./autogen.sh
-    if [ $? -ne 0 ]; then error; fi
-    ./configure --disable-tests
-    if [ $? -ne 0 ]; then error; fi
-    make HOST=x86_64-w64-mingw32 -j4
-	if [ $? -ne 0 ]; then error; fi
-}
+	   message "Preparing HOST mingw32 to build ...this may take a few minutes..."
+        cd depends && make HOST=x86_64-w64-mingw32
+           if [ $? -ne 0 ]; then error; fi
+           message "Install libdb4.8 (Berkeley DB4)"
+        cd .. && CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site
+           if [ $? -ne 0 ]; then error; fi
+    	   message "Build LUX...this may take a few minutes..."
+        ./autogen.sh
+           if [ $? -ne 0 ]; then error; fi
+        ./configure --prefix=`pwd`/depends/x86_64-w64-mingw32 --disable-tests
+           if [ $? -ne 0 ]; then error; fi
+        make HOST=x86_64-w64-mingw32 -j4
+           if [ $? -ne 0 ]; then error; fi
+           }
 
 createconf() {
 	#TODO: Can check for flag and skip this
 	#TODO: Random generate LUX user/password
 
-	message "Creating lux.conf..."
+	   message "Creating lux.conf..."
 	CONFDIR=~/.lux
 	CONFILE=$CONFDIR/lux.conf
-	if [ ! -d "$CONFDIR" ]; then mkdir $CONFDIR; fi
-	if [ $? -ne 0 ]; then error; fi	
+	   if [ ! -d "$CONFDIR" ]; then mkdir $CONFDIR; fi
+	   if [ $? -ne 0 ]; then error; fi	
 	mnip=$(curl -s https://api.ipify.org)
 	rpcuser=$(date +%s | sha256sum | base64 | head -c 10 ; echo)
 	rpcpass=$(openssl rand -base64 32)
