8a266bbcecbb9e80f593ed490f22b791f4894978
216k155 <32459326+216k155@users.noreply.github.com>
2018-01-09 10:56:21 +1100

    Fixed genbuild, git describe error

--- share/genbuild.sh
+++ share/genbuild.sh
@@ -21,7 +21,7 @@ if [ -e "$(which git 2>/dev/null)" -a "$(git rev-parse --is-inside-work-tree 2>/
     git diff >/dev/null 2>/dev/null 
 
     # if latest commit is tagged and not dirty, then override using the tag name
-    RAWDESC=$(git describe --abbrev=0 2>/dev/null)
+    RAWDESC=$(git describe --tags --abbrev=0 2>/dev/null)
     if [ "$(git rev-parse HEAD)" = "$(git rev-list -1 $RAWDESC)" ]; then
         git diff-index --quiet HEAD -- && DESC=$RAWDESC
     fi
