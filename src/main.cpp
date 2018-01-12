b080ad527a3a975229d21333ff94b3f19d38569f
216k155 <216k155@luxcore.io>
2018-01-13 05:09:03 +1100

    Fixed CheckBlockHeader

--- src/main.cpp
+++ src/main.cpp
@@ -3088,7 +3088,7 @@ bool CheckBlock(const CBlock& block, CValidationState& state, bool fCheckPOW, bo
 
     // Check that the header is valid (particularly PoW).  This is mostly
     // redundant with the call in AcceptBlockHeader.
-    if (block.IsProofOfWork() && !CheckBlockHeader(block, state, fCheckPOW))
+    if (!CheckBlockHeader(block, state, fCheckPOW))
         return state.DoS(100, error("CheckBlock() : CheckBlockHeader failed"),
             REJECT_INVALID, "bad-header", true);
 
