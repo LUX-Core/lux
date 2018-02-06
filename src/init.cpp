11a5e9457807a406c7066498a06fb18bd8a963e8
Duzy Chan <code@duzy.info>
2018-02-07 06:29:23 +0800

    Accepts zero mintxfee from command line

--- src/init.cpp
+++ src/init.cpp
@@ -774,7 +774,7 @@ bool AppInit2(boost::thread_group& threadGroup)
 #ifdef ENABLE_WALLET
     if (mapArgs.count("-mintxfee")) {
         CAmount n = 0;
-        if (ParseMoney(mapArgs["-mintxfee"], n) && n > 0)
+        if (ParseMoney(mapArgs["-mintxfee"], n) && n >= 0)
             CWallet::minTxFee = CFeeRate(n);
         else
             return InitError(strprintf(_("Invalid amount for -mintxfee=<amount>: '%s'"), mapArgs["-mintxfee"]));
