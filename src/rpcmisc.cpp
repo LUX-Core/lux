83836332a0ba89885c345f5b6e864df6405d8fc3
Duzy Chan <code@duzy.info>
2018-02-07 18:26:18 +0800

    Changed staking status

--- src/rpcmisc.cpp
+++ src/rpcmisc.cpp
@@ -105,7 +105,7 @@ Value getinfo(const Array& params, bool fHelp)
     obj.push_back(Pair("paytxfee", ValueFromAmount(payTxFee.GetFeePerK())));
 #endif
     obj.push_back(Pair("relayfee", ValueFromAmount(::minRelayTxFee.GetFeePerK())));
-    obj.push_back(Pair("staking status", (stake->IsActive() ? "Staking Active" : "Staking Not Active")));
+    obj.push_back(Pair("staking", (stake->IsActive() ? "activated" : "deactivated")));
     obj.push_back(Pair("errors", GetWarnings("statusbar")));
     return obj;
 }
