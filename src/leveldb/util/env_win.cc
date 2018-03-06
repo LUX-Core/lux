b1de4829633a3de1d8de4286af7648209155314d
tran <nguyenhoangtran11@yahoo.com>
2018-03-07 05:45:47 +1100

    Fix corrupted message on win OS
    (cherry picked from commit b9b8c9b)

--- src/leveldb/util/env_win.cc
+++ src/leveldb/util/env_win.cc
@@ -724,6 +724,7 @@ void Win32Logger::Logv( const char* format, va_list ap )
         // Print the message
         if (p < limit) {
             va_list backup_ap;
+            va_start (backup_ap, format);
             va_copy(backup_ap, ap);
             p += vsnprintf(p, limit - p, format, backup_ap);
             va_end(backup_ap);
