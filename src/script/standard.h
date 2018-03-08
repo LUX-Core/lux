38289f5159f71101d7c6aeda5958e1d4cca0435f
216k155 <216k155@luxcore.io>
2018-03-09 05:07:56 +1100

    added comment for MAX_OP_RETURN_RELAY = 83

--- src/script/standard.h
+++ src/script/standard.h
@@ -32,7 +32,7 @@ public:
     CScriptID(const uint160& in) : uint160(in) {}
 };
 
-static const unsigned int MAX_OP_RETURN_RELAY = 83;
+static const unsigned int MAX_OP_RETURN_RELAY = 83;  //!< bytes (+1 for OP_RETURN, +2 for the pushdata opcodes)
 extern unsigned nMaxDatacarrierBytes;
 
 /**
