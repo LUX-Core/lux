94c3405c54e905900d1e8d78bfa21d49f892a322
216k155 <216k155@luxcore.io>
2018-03-14 04:20:06 +1100

    Global secp256k1 object used for verification

--- src/pubkey.h
+++ src/pubkey.h
@@ -213,4 +213,15 @@ struct CExtPubKey {
     bool Derive(CExtPubKey& out, unsigned int nChild) const;
 };
 
+/** Users of this module must hold an ECCVerifyHandle. The constructor and
+ *  destructor of these are not allowed to run in parallel, though. */
+class ECCVerifyHandle
+{
+    static int refcount;
+
+public:
+    ECCVerifyHandle();
+    ~ECCVerifyHandle();
+};
+
 #endif // BITCOIN_PUBKEY_H
