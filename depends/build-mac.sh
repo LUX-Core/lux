41641f297c006c7aa34b88b7fe7d29e837deef61
Eric Juta <ericjohn.juta@consensys.net>
2018-03-18 02:53:05 +0000

    Update build-mac.sh

--- depends/build-mac.sh
+++ depends/build-mac.sh
@@ -1,8 +1,6 @@
 # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 echo "Please install homebrew first too"
-brew install autoconf automake pkg-config libtool librsvg cryptopp
-brew install autoconf automake berkeley-db4 libtool boost miniupnpc openssl pkg-config protobuf qt5 libzmq
-brew install cmake automake berkeley-db4 leveldb libtool boost --c++11 --without-single --without-static miniupnpc openssl pkg-config protobuf qt5 libevent imagemagick --with-librsvg
+brew install autoconf automake berkeley-db4 cmake leveldb libtool libzmq boost --c++11 --without-single --without-static miniupnpc openssl pkg-config protobuf qt5 libevent imagemagick --with-librsvg
 export MAKEJOBS=-j4
 export HOST="x86_64-apple-darwin11"
 export BITCOIN_CONFIG="--enable-reduce-exports --disable-tests"
@@ -11,7 +9,7 @@ export GOAL="deploy"
 mkdir -p depends/SDKs depends/sdk-sources
 cd depends/SDKs
 curl -sL https://github.com/phracker/MacOSX-SDKs/releases/download/MacOSX10.11.sdk/MacOSX10.11.sdk.tar.xz | tar xJ
-cd ..
+cd ../..
 make -C depends HOST=$HOST
 export TRAVIS_COMMIT_LOG=`git log --format=fuller -1`
 export BASE_OUTDIR=$(pwd)/out
