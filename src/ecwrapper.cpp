52c3f6b3116963aea5ee2373cf64aff5ff672f47
216k155 <216k155@luxcore.io>
2018-03-24 06:51:41 -0700

    Allow for OpenSSL 1.1 compilation

--- src/ecwrapper.cpp
+++ src/ecwrapper.cpp
@@ -18,138 +18,155 @@ namespace
  * recid selects which key is recovered
  * if check is non-zero, additional checks are performed
  */
-int ECDSA_SIG_recover_key_GFp(EC_KEY* eckey, ECDSA_SIG* ecsig, const unsigned char* msg, int msglen, int recid, int check)
-{
-    if (!eckey) return 0;
+    int ECDSA_SIG_recover_key_GFp(EC_KEY* eckey, ECDSA_SIG* ecsig, const unsigned char* msg, int msglen, int recid, int check)
+    {
+        if (!eckey) return 0;
 
-    int ret = 0;
-    BN_CTX* ctx = NULL;
+        int ret = 0;
+        BN_CTX* ctx = NULL;
 
-    BIGNUM* x = NULL;
-    BIGNUM* e = NULL;
-    BIGNUM* order = NULL;
-    BIGNUM* sor = NULL;
-    BIGNUM* eor = NULL;
-    BIGNUM* field = NULL;
-    EC_POINT* R = NULL;
-    EC_POINT* O = NULL;
-    EC_POINT* Q = NULL;
-    BIGNUM* rr = NULL;
-    BIGNUM* zero = NULL;
-    int n = 0;
-    int i = recid / 2;
+        BIGNUM* x = NULL;
+        BIGNUM* e = NULL;
+        BIGNUM* order = NULL;
+        BIGNUM* sor = NULL;
+        BIGNUM* eor = NULL;
+        BIGNUM* field = NULL;
+        EC_POINT* R = NULL;
+        EC_POINT* O = NULL;
+        EC_POINT* Q = NULL;
+        BIGNUM* rr = NULL;
+        BIGNUM* zero = NULL;
+        int n = 0;
+        int i = recid / 2;
 
-    const EC_GROUP* group = EC_KEY_get0_group(eckey);
-    if ((ctx = BN_CTX_new()) == NULL) {
-        ret = -1;
-        goto err;
-    }
-    BN_CTX_start(ctx);
-    order = BN_CTX_get(ctx);
-    if (!EC_GROUP_get_order(group, order, ctx)) {
-        ret = -2;
-        goto err;
-    }
-    x = BN_CTX_get(ctx);
-    if (!BN_copy(x, order)) {
-        ret = -1;
-        goto err;
-    }
-    if (!BN_mul_word(x, i)) {
-        ret = -1;
-        goto err;
-    }
-    if (!BN_add(x, x, ecsig->r)) {
-        ret = -1;
-        goto err;
-    }
-    field = BN_CTX_get(ctx);
-    if (!EC_GROUP_get_curve_GFp(group, field, NULL, NULL, ctx)) {
-        ret = -2;
-        goto err;
-    }
-    if (BN_cmp(x, field) >= 0) {
-        ret = 0;
-        goto err;
-    }
-    if ((R = EC_POINT_new(group)) == NULL) {
-        ret = -2;
-        goto err;
-    }
-    if (!EC_POINT_set_compressed_coordinates_GFp(group, R, x, recid % 2, ctx)) {
-        ret = 0;
-        goto err;
-    }
-    if (check) {
-        if ((O = EC_POINT_new(group)) == NULL) {
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+        const BIGNUM *sig_r, *sig_s;
+    ECDSA_SIG_get0(ecsig, &sig_r, &sig_s);
+#endif
+
+        const EC_GROUP* group = EC_KEY_get0_group(eckey);
+        if ((ctx = BN_CTX_new()) == NULL) {
+            ret = -1;
+            goto err;
+        }
+        BN_CTX_start(ctx);
+        order = BN_CTX_get(ctx);
+        if (!EC_GROUP_get_order(group, order, ctx)) {
             ret = -2;
             goto err;
         }
-        if (!EC_POINT_mul(group, O, NULL, R, order, ctx)) {
+        x = BN_CTX_get(ctx);
+        if (!BN_copy(x, order)) {
+            ret = -1;
+            goto err;
+        }
+        if (!BN_mul_word(x, i)) {
+            ret = -1;
+            goto err;
+        }
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+        if (!BN_add(x, x, sig_r)) {
+#else
+        if (!BN_add(x, x, ecsig->r)) {
+#endif
+            ret = -1;
+            goto err;
+        }
+        field = BN_CTX_get(ctx);
+        if (!EC_GROUP_get_curve_GFp(group, field, NULL, NULL, ctx)) {
             ret = -2;
             goto err;
         }
-        if (!EC_POINT_is_at_infinity(group, O)) {
+        if (BN_cmp(x, field) >= 0) {
             ret = 0;
             goto err;
         }
-    }
-    if ((Q = EC_POINT_new(group)) == NULL) {
-        ret = -2;
-        goto err;
-    }
-    n = EC_GROUP_get_degree(group);
-    e = BN_CTX_get(ctx);
-    if (!BN_bin2bn(msg, msglen, e)) {
-        ret = -1;
-        goto err;
-    }
-    if (8 * msglen > n) BN_rshift(e, e, 8 - (n & 7));
-    zero = BN_CTX_get(ctx);
-    if (!BN_zero(zero)) {
-        ret = -1;
-        goto err;
-    }
-    if (!BN_mod_sub(e, zero, e, order, ctx)) {
-        ret = -1;
-        goto err;
-    }
-    rr = BN_CTX_get(ctx);
-    if (!BN_mod_inverse(rr, ecsig->r, order, ctx)) {
-        ret = -1;
-        goto err;
-    }
-    sor = BN_CTX_get(ctx);
-    if (!BN_mod_mul(sor, ecsig->s, rr, order, ctx)) {
-        ret = -1;
-        goto err;
-    }
-    eor = BN_CTX_get(ctx);
-    if (!BN_mod_mul(eor, e, rr, order, ctx)) {
-        ret = -1;
-        goto err;
-    }
-    if (!EC_POINT_mul(group, Q, eor, R, sor, ctx)) {
-        ret = -2;
-        goto err;
-    }
-    if (!EC_KEY_set_public_key(eckey, Q)) {
-        ret = -2;
-        goto err;
-    }
+        if ((R = EC_POINT_new(group)) == NULL) {
+            ret = -2;
+            goto err;
+        }
+        if (!EC_POINT_set_compressed_coordinates_GFp(group, R, x, recid % 2, ctx)) {
+            ret = 0;
+            goto err;
+        }
+        if (check) {
+            if ((O = EC_POINT_new(group)) == NULL) {
+                ret = -2;
+                goto err;
+            }
+            if (!EC_POINT_mul(group, O, NULL, R, order, ctx)) {
+                ret = -2;
+                goto err;
+            }
+            if (!EC_POINT_is_at_infinity(group, O)) {
+                ret = 0;
+                goto err;
+            }
+        }
+        if ((Q = EC_POINT_new(group)) == NULL) {
+            ret = -2;
+            goto err;
+        }
+        n = EC_GROUP_get_degree(group);
+        e = BN_CTX_get(ctx);
+        if (!BN_bin2bn(msg, msglen, e)) {
+            ret = -1;
+            goto err;
+        }
+        if (8 * msglen > n) BN_rshift(e, e, 8 - (n & 7));
+        zero = BN_CTX_get(ctx);
+        if (!BN_zero(zero)) {
+            ret = -1;
+            goto err;
+        }
+        if (!BN_mod_sub(e, zero, e, order, ctx)) {
+            ret = -1;
+            goto err;
+        }
+        rr = BN_CTX_get(ctx);
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+        if (!BN_mod_inverse(rr, sig_r, order, ctx)) {
+#else
+        if (!BN_mod_inverse(rr, ecsig->r, order, ctx)) {
+#endif
+            ret = -1;
+            goto err;
+        }
+        sor = BN_CTX_get(ctx);
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+        if (!BN_mod_mul(sor, sig_s, rr, order, ctx)) {
+#else
+        if (!BN_mod_mul(sor, ecsig->s, rr, order, ctx)) {
+#endif
+            ret = -1;
+            goto err;
+        }
+        eor = BN_CTX_get(ctx);
+        if (!BN_mod_mul(eor, e, rr, order, ctx)) {
+            ret = -1;
+            goto err;
+        }
+        if (!EC_POINT_mul(group, Q, eor, R, sor, ctx)) {
+            ret = -2;
+            goto err;
+        }
+        if (!EC_KEY_set_public_key(eckey, Q)) {
+            ret = -2;
+            goto err;
+        }
 
-    ret = 1;
+        ret = 1;
 
-err:
-    if (ctx) {
-        BN_CTX_end(ctx);
-        BN_CTX_free(ctx);
+        err:
+        if (ctx) {
+            BN_CTX_end(ctx);
+            BN_CTX_free(ctx);
+        }
+        if (R != NULL) EC_POINT_free(R);
+        if (O != NULL) EC_POINT_free(O);
+        if (Q != NULL) EC_POINT_free(Q);
+        return ret;
     }
-    if (R != NULL) EC_POINT_free(R);
-    if (O != NULL) EC_POINT_free(O);
-    if (Q != NULL) EC_POINT_free(Q);
-    return ret;
-}
 
 } // anon namespace
 
@@ -218,8 +235,20 @@ bool CECKey::Recover(const uint256& hash, const unsigned char* p64, int rec)
     if (rec < 0 || rec >= 3)
         return false;
     ECDSA_SIG* sig = ECDSA_SIG_new();
-    BN_bin2bn(&p64[0], 32, sig->r);
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+    BIGNUM *sig_r = NULL;
+    BIGNUM *sig_s = NULL;
+    if (!(sig_r = BN_bin2bn(&p64[0],  32, nullptr)) ||
+        !(sig_s = BN_bin2bn(&p64[32], 32, nullptr)) ||
+	!ECDSA_SIG_set0(sig, sig_r, sig_s)) {
+	    BN_free(sig_r);
+	    BN_free(sig_s);
+	    return false;
+	}
+#else
+    BN_bin2bn(&p64[0],  32, sig->r);
     BN_bin2bn(&p64[32], 32, sig->s);
+#endif
     bool ret = ECDSA_SIG_recover_key_GFp(pkey, sig, (unsigned char*)&hash, sizeof(hash), rec, 0) == 1;
     ECDSA_SIG_free(sig);
     return ret;
