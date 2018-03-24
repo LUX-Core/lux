ee8f9a4db800f905f904998e1b3bbe79f1c620f5
tran <nguyenhoangtran11@yahoo.com>
2018-03-24 22:07:15 +0700

    Fix 'System error while flushing' issue

--- src/main.cpp
+++ src/main.cpp
@@ -48,6 +48,8 @@ using namespace std;
 #  define DEBUG_DUMP_STAKING_INFO_AddToBlockIndex() (void)0
 #endif
 
+#define MAX_DATA_FLUSH_RETRY 10
+
 const int LAST_HEIGHT_FEE_BLOCK = 180000;
 
 /**
@@ -2297,51 +2299,68 @@ static bool FlushStateToDisk(CValidationState& state, FlushStateMode mode)
 {
     LOCK(cs_main);
     static int64_t nLastWrite = 0;
-    try {
-        if ((mode == FLUSH_STATE_ALWAYS) ||
-            ((mode == FLUSH_STATE_PERIODIC || mode == FLUSH_STATE_IF_NEEDED) && pcoinsTip->GetCacheSize() > nCoinCacheSize) ||
-            (mode == FLUSH_STATE_PERIODIC && GetTimeMicros() > nLastWrite + DATABASE_WRITE_INTERVAL * 1000000)) {
-            // Typical CCoins structures on disk are around 100 bytes in size.
-            // Pushing a new one to the database can cause it to be written
-            // twice (once in the log, and once in the tables). This is already
-            // an overestimation, as most will delete an existing entry or
-            // overwrite one. Still, use a conservative safety factor of 2.
-            if (!CheckDiskSpace(100 * 2 * 2 * pcoinsTip->GetCacheSize()))
-                return state.Error("out of disk space");
-            // First make sure all block and undo data is flushed to disk.
-            FlushBlockFile();
-            // Then update all block file information (which may refer to block and undo files).
-            bool fileschanged = false;
-            for (set<int>::iterator it = setDirtyFileInfo.begin(); it != setDirtyFileInfo.end();) {
-                if (!pblocktree->WriteBlockFileInfo(*it, vinfoBlockFile[*it])) {
-                    return state.Abort("Failed to write to block index");
-                }
-                fileschanged = true;
-                setDirtyFileInfo.erase(it++);
-            }
-            if (fileschanged && !pblocktree->WriteLastBlockFile(nLastBlockFile)) {
-                return state.Abort("Failed to write to block index");
-            }
-            for (set<CBlockIndex*>::iterator it = setDirtyBlockIndex.begin(); it != setDirtyBlockIndex.end();) {
-                if (!pblocktree->WriteBlockIndex(CDiskBlockIndex(*it))) {
-                    return state.Abort("Failed to write to block index");
-                }
-                setDirtyBlockIndex.erase(it++);
-            }
-            pblocktree->Sync();
-            // Finally flush the chainstate (which may refer to block index entries).
-            if (!pcoinsTip->Flush())
-                return state.Abort("Failed to write to coin database");
-            // Update best block in wallet (so we can detect restored wallets).
-            if (mode != FLUSH_STATE_IF_NEEDED) {
-                g_signals.SetBestChain(chainActive.GetLocator());
-            }
-            nLastWrite = GetTimeMicros();
-        }
-    } catch (const std::runtime_error& e) {
-        return state.Abort(std::string("System error while flushing: ") + e.what());
+    int retries = MAX_DATA_FLUSH_RETRY;
+    string strErr = "";
+
+    while (retries > 0)
+    {
+		bool isExceptionOccured = false;
+		try {
+		    if ((mode == FLUSH_STATE_ALWAYS) ||
+		        ((mode == FLUSH_STATE_PERIODIC || mode == FLUSH_STATE_IF_NEEDED) && pcoinsTip->GetCacheSize() > nCoinCacheSize) ||
+		        (mode == FLUSH_STATE_PERIODIC && GetTimeMicros() > nLastWrite + DATABASE_WRITE_INTERVAL * 1000000)) {
+		        // Typical CCoins structures on disk are around 100 bytes in size.
+		        // Pushing a new one to the database can cause it to be written
+		        // twice (once in the log, and once in the tables). This is already
+		        // an overestimation, as most will delete an existing entry or
+		        // overwrite one. Still, use a conservative safety factor of 2.
+		        if (!CheckDiskSpace(100 * 2 * 2 * pcoinsTip->GetCacheSize()))
+		            return state.Error("out of disk space");
+		        // First make sure all block and undo data is flushed to disk.
+		        FlushBlockFile();
+		        // Then update all block file information (which may refer to block and undo files).
+		        bool fileschanged = false;
+		        for (set<int>::iterator it = setDirtyFileInfo.begin(); it != setDirtyFileInfo.end();) {
+		            if (!pblocktree->WriteBlockFileInfo(*it, vinfoBlockFile[*it])) {
+		                return state.Abort("Failed to write to block index");
+		            }
+		            fileschanged = true;
+		            setDirtyFileInfo.erase(it++);
+		        }
+		        if (fileschanged && !pblocktree->WriteLastBlockFile(nLastBlockFile)) {
+		            return state.Abort("Failed to write to block index");
+		        }
+		        for (set<CBlockIndex*>::iterator it = setDirtyBlockIndex.begin(); it != setDirtyBlockIndex.end();) {
+		            if (!pblocktree->WriteBlockIndex(CDiskBlockIndex(*it))) {
+		                return state.Abort("Failed to write to block index");
+		            }
+		            setDirtyBlockIndex.erase(it++);
+		        }
+		        pblocktree->Sync();
+		        // Finally flush the chainstate (which may refer to block index entries).
+		        if (!pcoinsTip->Flush())
+		            return state.Abort("Failed to write to coin database");
+		        // Update best block in wallet (so we can detect restored wallets).
+		        if (mode != FLUSH_STATE_IF_NEEDED) {
+		            g_signals.SetBestChain(chainActive.GetLocator());
+		        }
+		        nLastWrite = GetTimeMicros();
+		    }
+		} catch (const std::runtime_error& e) {
+		    isExceptionOccured = true;
+
+		    strErr = strprintf("System error while flushing: %s", e.what());
+		    LogPrintf("%s\n", strErr);
+		}
+
+		if (!isExceptionOccured)
+		{
+			return true;
+		}
+		retries--;
     }
-    return true;
+
+    return state.Abort(strErr);
 }
 
 void FlushStateToDisk()
