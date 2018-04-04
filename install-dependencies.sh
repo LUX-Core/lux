869e2af56639cdb8693bc0885eab4283d06bca56
tran <nguyenhoangtran11@yahoo.com>
2018-04-05 02:28:23 +0700

    Update the script to set up env correctly
    Signed-off-by: tran <nguyenhoangtran11@yahoo.com>

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 
-set -e
-
 date
 
 #################################################################
 # Update Ubuntu and install prerequisites for running LUX   #
 #################################################################
 sudo apt-get update
+
+#################################################################
+# Lux QT wallet are using QT5. So, QT4 must be removed
+#################################################################
+sudo apt purge qt4-qmake libqt4-dev -y
+
+#################################################################
+# Remove incompatible libdb
+#################################################################
+sudo apt purge libdb++-dev libdb-dev -y
+
+#################################################################
+# Remove unused package
+#################################################################
+sudo apt autoremove -y
+
 #################################################################
 # Build LUX from source                                     #
 #################################################################
 NPROC=$(nproc)
 echo "nproc: $NPROC"
+
+set -e
+
 #################################################################
 # Install all necessary packages for building LUX         #
 #################################################################
-sudo apt-get install -y qt4-qmake libqt4-dev libminiupnpc-dev libdb++-dev libdb-dev libcrypto++-dev libboost-all-dev build-essential libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libssl-dev libdb++-dev libssl-dev ufw git software-properties-common build-essential libtool autotools-dev autoconf pkg-config libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libqrencode-dev automake g++-mingw-w64-x86-64 libevent-dev
-
+sudo apt-get install -y libdb4.8-dev libdb4.8++-dev libzmq3-dev libminiupnpc-dev libcrypto++-dev libboost-all-dev build-essential libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libssl-dev ufw git software-properties-common libtool autotools-dev autoconf pkg-config libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libqrencode-dev automake g++-mingw-w64-x86-64 libevent-dev
 
