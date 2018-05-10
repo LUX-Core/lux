45dd43de8252ab0aee3605ab61449076fd318793
216k155 <216k155@luxcore.io>
2018-05-10 19:49:31 +1000

    Fixed bad_alloc walletdb cache issue

--- src/walletdb.cpp
+++ src/walletdb.cpp
@@ -133,19 +133,7 @@ bool CWalletDB::WriteMasterKey(unsigned int nID, const CMasterKey& kMasterKey)
 bool CWalletDB::WriteCScript(const uint160& hash, const CScript& redeemScript)
 {
     nWalletDBUpdated++;
-    std::pair<std::string, uint160> pair = std::make_pair(std::string("cscript"), hash);
-
-    try {
-        if (!Write(pair, redeemScript, false)) {
-            std::cout << "Failed to write script to WalletDB" << std::endl;
-            return false;
-        }
-    } catch(std::bad_alloc e) {
-        std::cout << "bad_alloc in CWalletDB::WriteCScript::Write" << std::endl;
-        return false;
-    }
-
-    return true;
+    return Write(std::make_pair(std::string("cscript"), hash), *(const CScriptBase*)(&redeemScript), false);
 }
 
 bool CWalletDB::WriteWatchOnly(const CScript& dest)
