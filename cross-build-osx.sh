917cf559717dc3d35267d04c693ae79d34e693a3
216k155 <216k155@luxcore.io>
2018-05-13 12:30:13 +1000

    Add build-linux and cross-build-osx, qt patchs, zeromq patch

---
+++ cross-build-osx.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#-###############################################-#
+# C++ Cross-Compiler - The Luxcore Developer-2018 #
+#-###############################################-#
+
+# Set platform variables
+export HOST="x86_64-apple-darwin11"
+export OSX_SDK="10.11"
+
+# curl OSX-SDK
+mkdir -p depends/SDKs depends/sdk-sources
+cd depends/SDKs
+curl -sL https://github.com/phracker/MacOSX-SDKs/releases/download/MacOSX10.11.sdk/MacOSX10.11.sdk.tar.xz | tar xJ
+cd ../..
+
+# Install development tools if needed
+if [ ! -f $CC ]; then
+  sudo apt-get install curl librsvg2-bin libtiff-tools bsdmainutils cmake imagemagick libcap-dev libz-dev libbz2-dev python-setuptools
+fi
+
+# Make dependencies
+make -C depends HOST=$HOST
+cd ../..
+./autogen.sh && ./configure --prefix=$PWD/depends/$PLATFORM --host=$PLATFORM --disable-tests && make -j$(nproc) && make deploy
+
