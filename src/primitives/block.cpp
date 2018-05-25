f544a1553729a00567e4709648ac3407343fb57f
216k155 <216k155@luxcore.io>
2018-05-25 09:48:26 +0700

    Fix block's hash calculating issue

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -16,9 +16,9 @@
 
 uint256 CBlockHeader::GetHash(bool phi2block) const {
     if (nVersion > VERSIONBITS_LAST_OLD_BLOCK_VERSION && phi2block) {
-        return phi2_hash(BEGIN(nVersion), BEGIN(nNonce));
+        return phi2_hash(BEGIN(nVersion), END(nNonce));
     } else {
-        return Phi1612(BEGIN(nVersion), BEGIN(nNonce));
+        return Phi1612(BEGIN(nVersion), END(nNonce));
     }
 }
 
