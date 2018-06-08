6646a4c6d60b2d403f5c7ac3b244ab220b3971bd
216k155 <216k155@luxcore.io>
2018-06-08 17:22:19 +0700

    Add logging for reward split in PoW block

--- src/miner.cpp
+++ src/miner.cpp
@@ -338,6 +338,10 @@ std::unique_ptr<CBlockTemplate> BlockAssembler::CreateNewBlock(const CScript& sc
             coinbaseTx.vout[1].scriptPubKey = mnPayee;
             coinbaseTx.vout[1].nValue = mnReward;
 
+            CTxDestination txDest;
+            ExtractDestination(mnPayee, txDest);
+            LogPrintf("%s: Masternode payment to %s (pow)\n", __func__, EncodeDestination(txDest));
+
             //miner's reward is everything that left after possibly inaccurate division
             minerReward = totalReward - mnReward;
         } else {
