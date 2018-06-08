0e63171e2acc43f5dd02dbe6482a26150c47bde3
216k155 <216k155@luxcore.io>
2018-06-08 18:11:51 +0700

    Set CSV start time same as SegWit, fix getblocktemplate not showing MNs
    Delete memory-only field payee in CBlock, which is not used now.
    Set CSV deployment for testnet, set forks block numbers with a space of
    200 blocks

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -151,7 +151,7 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1557187200; // TODO: ?? - just some random date - 05.07.2019
         //TODO: set CSV parameters for mainnet
         consensus.vDeployments[Consensus::DEPLOYMENT_CSV].bit = 0;
-        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nStartTime = 0; //TODO: ?
+        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nStartTime = 1529107200;
         consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nTimeout = 1557187200; // TODO: ?? - just some random date - 05.07.2019
 
         //SMART_CONTRACTS_HARDFORK deployment does not require start time and timeout, because it uses block number
@@ -275,8 +275,11 @@ public:
         consensus.nMinerConfirmationWindow = 1440; // nPowTargetTimespan / nPowTargetSpacing
         // Deployment of SegWit (BIP141 and BIP143)
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1577836800;
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1577836900; // Never / undefined
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 577836800;
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 2577836900; // Never / undefined
+        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].bit = 0;
+        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nStartTime = 577836800;
+        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nTimeout = 2577836900; // Never / undefined
         consensus.nLastPOWBlock = 6000000;
 
         networkID = CBaseChainParams::TESTNET;
@@ -321,12 +324,10 @@ public:
 //        std::cout << genesis.GetHash().GetHex() << std::endl;
 //        std::cout << genesis.hashMerkleRoot.GetHex() << std::endl;
 
-        //TODO: Phi2_hash hardfork block here !!!
-        //nSwitchPhi2Block = 300000;
-        //nFirstSCBlock = 300000;
-        //nPruneAfterHeight = 100000;
-
-        nSplitRewardBlock = 1000;
+        nSplitRewardBlock = 100;
+        nSwitchPhi2Block = 300;
+        nFirstSCBlock = 500;
+        nPruneAfterHeight = 700;
 
         consensus.hashGenesisBlock = genesis.GetHash();
 
