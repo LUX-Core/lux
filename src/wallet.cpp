b58ffd85aeffa8aaecef48cda6f91919ad25722f
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2018-06-17 21:18:32 +1000

    wallet: prevent init messages after initial load
    (cherry picked from commit d47b2b9)
    (cherry picked from commit 91f35ef7a5ec1254699d31056076279b5c1ae4c5)

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -2856,8 +2856,10 @@ bool CWallet::TopUpKeyPool(unsigned int kpSize)
             setKeyPool.insert(nEnd);
             LogPrintf("keypool added key %d, size=%u\n", nEnd, setKeyPool.size());
             double dProgress = 100.f * nEnd / (nTargetSize + 1);
-            std::string strMsg = strprintf(_("Loading wallet... (%3.2f %%)"), dProgress);
-            uiInterface.InitMessage(strMsg);
+            if (dProgress < 100.1) {
+                std::string strMsg = strprintf(_("Loading wallet... (%3.2f %%)"), dProgress);
+                uiInterface.InitMessage(strMsg);
+            }
         }
     }
     return true;
