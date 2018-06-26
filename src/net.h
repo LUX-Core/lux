ea452d77d2fb6a1b185becc5431abed1966a212a
tran <nguyenhoangtran11@yahoo.com>
2018-06-27 02:19:46 +0700

    Fix: connection issue on Win and Mac
    Signed-off-by: tran <nguyenhoangtran11@yahoo.com>
    (cherry picked from commit 2a427cfe5fea3f52a9a2f63b870d16277dc073ad)
    (cherry picked from commit 93d5d0d67606161fbd50a062386e0ec7bbee6c54)

--- src/net.h
+++ src/net.h
@@ -623,7 +623,7 @@ public:
     {
         try {
             BeginMessage(pszCommand);
-            ssSend << a1 << (unsigned long)a2 << a3 << a4 << a5 << a6 << a7 << a8 << a9;
+            ssSend << a1 << a2 << a3 << a4 << a5 << a6 << a7 << a8 << a9;
             EndMessage();
         } catch (...) {
             AbortMessage();
