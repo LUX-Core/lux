6d86c2a6649b8c8428cf24957bc1759ca36ea844
tran <nguyenhoangtran11@yahoo.com>
2018-08-26 02:57:46 +1000

    Fix travis building issue
    Signed-off-by: tran <nguyenhoangtran11@yahoo.com>
    
    (cherry picked from commit 1cbcdf55b0fbc09df92c055cae8ee6d2f2131879)
    Signed-off-by: k155 <216k155@luxcore.io>

--- src/rpcmisc.cpp
+++ src/rpcmisc.cpp
@@ -144,9 +144,9 @@ UniValue getstateinfo(const UniValue& params, bool fHelp)
     LuxDGP luxDGP(globalState.get());
 
     UniValue obj(UniValue::VOBJ);
-    obj.push_back(Pair("maxblocksize", (int64_t)luxDGP.getBlockSize(chainActive.Height())));
-    obj.push_back(Pair("blockgaslimit", (int64_t)luxDGP.getMinGasPrice(chainActive.Height())));
-    obj.push_back(Pair("mingasprice", (int64_t)luxDGP.getBlockGasLimit(chainActive.Height())));
+    obj.push_back(Pair("maxblocksize", (int)luxDGP.getBlockSize(chainActive.Height())));
+    obj.push_back(Pair("blockgaslimit", (int)luxDGP.getMinGasPrice(chainActive.Height())));
+    obj.push_back(Pair("mingasprice", (int)luxDGP.getBlockGasLimit(chainActive.Height())));
 
     return obj;
 }
