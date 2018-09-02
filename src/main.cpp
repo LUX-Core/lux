6f6fd562f4e041c3d2085e903261ea7d945733a1
tran <nguyenhoangtran11@yahoo.com>
2018-09-03 01:37:22 +0700

    Refactor: Improve locking mechanism on AcceptToMemoryPool
    Signed-off-by: tran <nguyenhoangtran11@yahoo.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1059,7 +1059,7 @@ bool AcceptToMemoryPool(CTxMemPool& pool, CValidationState& state, const CTransa
         CAmount nValueIn = 0;
         LockPoints lp;
         {
-            LOCK(pool.cs);
+            //LOCK(pool.cs);
             CCoinsViewMemPool viewMemPool(pcoinsTip, pool);
             view.SetBackend(viewMemPool);
 
