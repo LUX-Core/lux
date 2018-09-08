29ab74d9841f85267409d137b243549f06d0db1d
216k155 <216k155@luxcore.io>
2018-09-09 09:33:42 +1000

    QT - remove unsual dummy data address
    Signed-off-by: 216k155 <216k155@luxcore.io>

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -105,7 +105,7 @@ QFont bitcoinAddressFont()
 #endif
     return font;
 }
-
+/*
 // Just some dummy data to generate an convincing random-looking (but consistent) address
     static const uint8_t dummydata[] = {0xeb, 0x15, 0x23, 0x1d, 0xfc, 0xeb, 0x60, 0x92, 0x58, 0x86, 0xb6, 0x7d, 0x06,
                                         0x52, 0x99, 0x92, 0x59, 0x15, 0xae, 0xb1, 0x72, 0xc0, 0x66, 0x47};
@@ -120,7 +120,7 @@ QFont bitcoinAddressFont()
         }
         return "";
     }
-
+*/
 void setupAddressWidget(QValidatedLineEdit* widget, QWidget* parent)
 {
     parent->setFocusProxy(widget);
@@ -129,8 +129,7 @@ void setupAddressWidget(QValidatedLineEdit* widget, QWidget* parent)
 #if QT_VERSION >= 0x040700
     // We don't want translators to use own addresses in translations
     // and this is the only place, where this address is supplied.
-        widget->setPlaceholderText(QObject::tr("Enter a Lux address (e.g. %1)").arg(
-        QString::fromStdString(DummyAddress(Params()))));
+     widget->setPlaceholderText(QObject::tr("Enter a Lux address (e.g. %1)").arg("LhfHsuK9Ekjj2dYoEy4wvEiQHvRtzZQnYa"));
 #endif
     widget->setValidator(new BitcoinAddressEntryValidator(parent));
     widget->setCheckValidator(new BitcoinAddressCheckValidator(parent));
