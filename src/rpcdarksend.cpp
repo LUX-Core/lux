6c6b12d33c2dee2c41b88a7d203efa54400094e9
Lev Sizov <levsizov2015@outlook.com>
2018-09-21 16:11:02 +0700

    Use random position for change output in the CreateTransaction

--- src/rpcdarksend.cpp
+++ src/rpcdarksend.cpp
@@ -44,7 +44,7 @@ void SendMoney(const CTxDestination& address, CAmount nValue, CWalletTx& wtxNew,
     // Create and send the transaction
     CReserveKey reservekey(pwalletMain);
     CAmount nFeeRequired;
-    int nChangePos;
+    int nChangePos = -1;
     if (!pwalletMain->CreateTransaction(scriptPubKey, nValue, wtxNew, reservekey, nFeeRequired, nChangePos, strError, NULL, coin_type)) {
         if (nValue + nFeeRequired > pwalletMain->GetBalance())
             strError = strprintf("Error: This transaction requires a transaction fee of at least %s because of its amount, complexity, or use of recently received funds!", FormatMoney(nFeeRequired));
