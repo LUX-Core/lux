15143ceb4fdda6df37449dae8abe48ae9cba16da
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2018-10-01 03:52:04 +1000

    Coins - restore viewcache code indentation
    was very hard to read like that...
    
    see commit c0a2772ae186c9f1d2067679e4e9eec835a230f5
    
    Signed-off-by: 216k155 <216k155@luxcore.io>

--- src/coins.cpp
+++ src/coins.cpp
@@ -219,38 +219,41 @@ bool CCoinsViewCache::BatchWrite(CCoinsMap& mapCoins, const uint256& hashBlockIn
 {
     assert(!hasModifier);
     for (CCoinsMap::iterator it = mapCoins.begin(); it != mapCoins.end(); it = mapCoins.erase(it)) {
-        if (!(it->second.flags & CCoinsCacheEntry::DIRTY)) { continue; }
-            CCoinsMap::iterator itUs = cacheCoins.find(it->first);
-            if (itUs == cacheCoins.end()) {
-                if (!(it->second.flags & CCoinsCacheEntry::FRESH && it->second.coins.IsPruned())) {
-                    // The parent cache does not have an entry, while the child
-                    // cache does have (a non-pruned) one. Move the data up, and
-                    // mark it as fresh (if the grandparent did have it, we
-                    // would have pulled it in at first GetCoins).
-                    CCoinsCacheEntry& entry = cacheCoins[it->first];
-                    entry.coins.swap(it->second.coins);
-                    cachedCoinsUsage += entry.coins.DynamicMemoryUsage();
-                    entry.flags = CCoinsCacheEntry::DIRTY;
-                    if (it->second.flags & CCoinsCacheEntry::FRESH) {
-                        entry.flags |= CCoinsCacheEntry::FRESH;
-                    }
+
+        if (!(it->second.flags & CCoinsCacheEntry::DIRTY))
+            continue; // Ignore non-dirty entries
+
+        CCoinsMap::iterator itUs = cacheCoins.find(it->first);
+        if (itUs == cacheCoins.end()) {
+            if (!((it->second.flags & CCoinsCacheEntry::FRESH) && it->second.coins.IsPruned())) {
+                // The parent cache does not have an entry, while the child
+                // cache does have (a non-pruned) one. Move the data up, and
+                // mark it as fresh (if the grandparent did have it, we
+                // would have pulled it in at first GetCoins).
+                CCoinsCacheEntry& entry = cacheCoins[it->first];
+                entry.coins.swap(it->second.coins);
+                cachedCoinsUsage += entry.coins.DynamicMemoryUsage();
+                entry.flags = CCoinsCacheEntry::DIRTY;
+                if (it->second.flags & CCoinsCacheEntry::FRESH) {
+                    entry.flags |= CCoinsCacheEntry::FRESH;
                 }
+            }
+        } else {
+            if ((it->second.flags & CCoinsCacheEntry::FRESH) && !itUs->second.coins.IsPruned()) {
+                throw std::logic_error("FRESH flag misapplied to cache entry for base transaction with spendable outputs");
+            }
+            if ((itUs->second.flags & CCoinsCacheEntry::FRESH) && it->second.coins.IsPruned()) {
+                // The grandparent does not have an entry, and the child is
+                // modified and being pruned. This means we can just delete
+                // it from the parent.
+                cachedCoinsUsage -= itUs->second.coins.DynamicMemoryUsage();
+                cacheCoins.erase(itUs);
             } else {
-                if ((it->second.flags & CCoinsCacheEntry::FRESH) && !itUs->second.coins.IsPruned()) {
-                    throw std::logic_error("FRESH flag misapplied to cache entry for base transaction with spendable outputs");
-                }
-                if ((itUs->second.flags & CCoinsCacheEntry::FRESH) && it->second.coins.IsPruned()) {
-                    // The grandparent does not have an entry, and the child is
-                    // modified and being pruned. This means we can just delete
-                    // it from the parent.
-                    cachedCoinsUsage -= itUs->second.coins.DynamicMemoryUsage();
-                    cacheCoins.erase(itUs);
-                } else {
-                    // A normal modification.
-                    cachedCoinsUsage -= itUs->second.coins.DynamicMemoryUsage();
-                    itUs->second.coins.swap(it->second.coins);
-                    cachedCoinsUsage += itUs->second.coins.DynamicMemoryUsage();
-                    itUs->second.flags |= CCoinsCacheEntry::DIRTY;
+                // A normal modification.
+                cachedCoinsUsage -= itUs->second.coins.DynamicMemoryUsage();
+                itUs->second.coins.swap(it->second.coins);
+                cachedCoinsUsage += itUs->second.coins.DynamicMemoryUsage();
+                itUs->second.flags |= CCoinsCacheEntry::DIRTY;
             }
         }
     }
