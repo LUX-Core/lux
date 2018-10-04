752f1ab068fce094cd3f1277ffefa0172258266c
Lev Sizov <levsizov2015@outlook.com>
2018-10-05 04:02:37 +0700

    [tests] Add missing zmq_pub_raw_tx_endpoint luxgate config entry

--- test/functional/luxgate_p2p.py
+++ test/functional/luxgate_p2p.py
@@ -79,7 +79,8 @@ class LuxgateTest(LuxTestFramework):
                         "host": "127.0.0.1",
                         "port": 8332, 
                         "rpcuser": "rpcuser",
-                        "rpcpassword": "rpcpassword"
+                        "rpcpassword": "rpcpassword",
+                        "zmq_pub_raw_tx_endpoint": "127.0.0.1:5000"
                     },
                 ]},
                 default=EncodeDecimal, ensure_ascii=True)
