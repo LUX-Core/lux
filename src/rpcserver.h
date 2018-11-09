57d34e84a88670c2eec00c664175f62f4a582fd8
216k155 <216k155@luxcore.io>
2018-11-09 14:43:45 +1100

    RPC - Add getchaintxstats (The average rate of transactions per second)
    Signed-off-by: k155 <216k155@luxcore.io>

--- src/rpcserver.h
+++ src/rpcserver.h
@@ -437,6 +437,7 @@ extern UniValue gettxoutsetinfo(const UniValue& params, bool fHelp);
 extern UniValue gettxout(const UniValue& params, bool fHelp);
 extern UniValue verifychain(const UniValue& params, bool fHelp);
 extern UniValue getchaintips(const UniValue& params, bool fHelp);
+extern UniValue getchaintxstats(const UniValue& params, bool fHelp);
 extern UniValue switchnetwork(const UniValue& params, bool fHelp);
 extern UniValue invalidateblock(const UniValue& params, bool fHelp);
 extern UniValue reconsiderblock(const UniValue& params, bool fHelp);
