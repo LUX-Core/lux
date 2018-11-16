2c90db33540f9b3efd0e6a00e8c5f49ac283a487
dvajdual <dvajdual@gmail.com>
2018-11-16 20:55:53 +0300

    QT:overview: increase size (wasn't visible 5 transactions)

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -145,7 +145,7 @@ BitcoinGUI::BitcoinGUI(const PlatformStyle *platformStyle, const NetworkStyle* n
 {
     /* Open CSS when configured */
     this->setStyleSheet(GUIUtil::loadStyleSheet());
-    resize(1200, 735);
+    resize(1200, 750);
     QString windowTitle = tr("Luxcore") + " - ";
 
 #ifdef ENABLE_UPDATER
