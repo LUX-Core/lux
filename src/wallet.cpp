9a58972112ab593e30fbb3c5cdb9c22914059a12
tran <nguyenhoangtran11@yahoo.com>
2018-11-25 08:00:34 +0700

    Improve lock mechanism on ScanForWalletTransactions
    ScanForWalletTransactions() => LOCK(cs_wallet) =>
    AddToWalletIfInvolvingMe() => MarkConflicted() =>
    LOCK2(cs_main, cs_wallet)
    
    Signed-off-by: tran <nguyenhoangtran11@yahoo.com>

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1675,7 +1675,7 @@ int CWallet::ScanForWalletTransactions(CBlockIndex* pindexStart, bool fUpdate)
             CBlock block;
             ReadBlockFromDisk(block, pindex, Params().GetConsensus());
             {
-                LOCK(cs_wallet);
+                //LOCK(cs_wallet);
                 for (CTransaction& tx : block.vtx) {
                     if (AddToWalletIfInvolvingMe(tx, &block, fUpdate))
                         ret++;
