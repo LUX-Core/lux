9401a65abe84a7e6083089b2a2ab75274f8d35e2
Lev Sizov <levsizov2015@outlook.com>
2018-11-26 16:35:46 +0100

    Fix not allowing PHI2 blocks in regtest using generate RPC method

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -3287,7 +3287,7 @@ UniValue generateBlocks(std::shared_ptr<CReserveScript> coinbaseScript, int nGen
             LOCK(cs_main);
             IncrementExtraNonce(pblock, chainActive.Tip(), nExtraNonce);
         }
-        while (nMaxTries > 0 && pblock->nNonce < nInnerLoopCount && !CheckProofOfWork(pblock->GetHash(), pblock->nBits, Params().GetConsensus())) {
+        while (nMaxTries > 0 && pblock->nNonce < nInnerLoopCount && !CheckProofOfWork(pblock->GetHash(nHeight + 1 > Params().SwitchPhi2Block()), pblock->nBits, Params().GetConsensus())) {
             ++pblock->nNonce;
             --nMaxTries;
         }
