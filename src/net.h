d8328639da712a00d424bdb5f55a88129f72a2b1
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2018-12-01 06:11:36 +0100

    net: fix node misbehaving typo

--- src/net.h
+++ src/net.h
@@ -275,7 +275,7 @@ public:
     {
         switch (banReason) {
             case BanReasonNodeMisbehaving:
-                return "node misbehabing";
+                return "node misbehaving";
             case BanReasonManuallyAdded:
                 return "manually added";
             default:
