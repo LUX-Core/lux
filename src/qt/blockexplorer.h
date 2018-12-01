57ea2e97e2c5bc3c3a08a5e7657dd79d7d848392
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2018-12-01 13:07:29 +0100

    QT: show current network icon in blockexplorer

--- src/qt/blockexplorer.h
+++ src/qt/blockexplorer.h
@@ -1,10 +1,18 @@
 #ifndef BLOCKEXPLORER_H
 #define BLOCKEXPLORER_H
 
+#include <QLabel>
 #include <QMainWindow>
+#include <QStatusBar>
 
+#include "bitcoinunits.h"
+#include "clientmodel.h"
 #include "base58.h"
+#include "guiconstants.h"
 #include "uint256.h"
+#include "optionsmodel.h"
+#include "platformstyle.h"
+
 #undef loop
 
 namespace Ui
@@ -16,6 +24,7 @@ class BlockExplorer;
 class CBlockIndex;
 class CTransaction;
 class CBlockTreeDB;
+class NetworkDisplayStatusBarControl;
 
 std::string getexplorerBlockHash(int64_t);
 const CBlockIndex* getexplorerBlockIndex(int64_t);
@@ -27,8 +36,10 @@ class BlockExplorer : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit BlockExplorer(QWidget* parent = 0);
+    explicit BlockExplorer(const PlatformStyle* platformStyle, QWidget* parent = 0);
     ~BlockExplorer();
+
+    void setClientModel(ClientModel *clientModel);
 
 protected:
     void keyPressEvent(QKeyEvent* event);
@@ -46,12 +57,32 @@ private:
     int m_HistoryIndex;
     QStringList m_History;
 
+    ClientModel *model;
+    NetworkDisplayStatusBarControl* unitDisplayControl;
+
     void setBlock(CBlockIndex* pBlock);
     bool switchTo(const QString& query);
     void setContent(const std::string& content);
     void updateNavButtons();
 
     void Translations();
+};
+
+class NetworkDisplayStatusBarControl : public QLabel
+{
+    Q_OBJECT
+
+public:
+    explicit NetworkDisplayStatusBarControl(const PlatformStyle* platformStyle);
+    /** Lets the control know about the Options Model (and its signals) */
+    void setOptionsModel(OptionsModel* optionsModel);
+
+private:
+    OptionsModel* optionsModel;
+
+private Q_SLOTS:
+    /** When Display Units are changed on OptionsModel it will refresh the display text of the control on the status bar */
+    void updateDisplayUnit(int newUnits);
 };
 
 #endif // BLOCKEXPLORER_H
