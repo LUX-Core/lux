1ad3e72c739dfd787dde382a5f1b7d5233e9c65c
enkayz <lord.nk@gmail.com>
2019-01-14 17:46:13 +1030

    don't update darksend ui when darksend is disabled

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -460,6 +460,19 @@ void OverviewPage::updateDarkSendProgress()
     QString strAmountAndRounds;
     QString strAnonymizeLuxAmount = BitcoinUnits::formatHtmlWithUnit(nDisplayUnit, nAnonymizeLuxAmount * COIN, false, BitcoinUnits::separatorAlways);
 
+    if (!fEnableDarksend) {
+        ui->darksendProgress->setValue(0);
+        ui->darksendProgress->setToolTip(tr("Darksend disabled"));
+
+        // when balance is zero just show info from settings
+        strAnonymizeLuxAmount = strAnonymizeLuxAmount.remove(strAnonymizeLuxAmount.indexOf("."), BitcoinUnits::decimals(nDisplayUnit) + 1);
+        strAmountAndRounds = strAnonymizeLuxAmount + " / " + tr("%n Rounds", "", nDarksendRounds);
+
+        ui->labelAmountRounds->setToolTip(tr("Darksend disabled"));
+        ui->labelAmountRounds->setText(tr("Disabled"));
+        return;
+    }
+
     if (currentBalance == 0) {
         ui->darksendProgress->setValue(0);
         ui->darksendProgress->setToolTip(tr("No inputs detected"));
