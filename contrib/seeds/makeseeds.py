c2f84f9397cc35cceba8d77655858c554ffefd3e
Giacomo Milligan <giaki3003@gmail.com>
2019-02-25 00:21:58 +0100

    [seeder] Changed version we're collecting (5.3.2)
    Thanks @tpruvot

--- contrib/seeds/makeseeds.py
+++ contrib/seeds/makeseeds.py
@@ -26,7 +26,7 @@ import collections
 PATTERN_IPV4 = re.compile(r"^((\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})):(\d+)$")
 PATTERN_IPV6 = re.compile(r"^\[([0-9a-z:]+)\]:(\d+)$")
 PATTERN_ONION = re.compile(r"^([abcdefghijklmnopqrstuvwxyz234567]{16}\.onion):(\d+)$")
-PATTERN_AGENT = re.compile(r"^(/LUXCore:5.3.(0|1|99)/)$")
+PATTERN_AGENT = re.compile(r"^(/LUXCore:5.3.2/)$")
 
 def parseline(line):
     sline = line.split()
