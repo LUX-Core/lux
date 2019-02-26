5e4adbafc58575d56a6427c29a01bbff289aa1ab
GrapeLUX <48026627+GrapeLUX@users.noreply.github.com>
2019-02-26 19:01:03 +0000

    optimasion of banned address check

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -608,39 +608,16 @@ uint256 CMasterNode::CalculateScore(int mod, int64_t nBlockHeight) {
     std::string bannedvin4 = "CTxIn(COutPoint(370186282e30d9a7bdf1744599573215bf56663cafb096444db6f0e1634eaac7, 1), scriptSig=)";
     std::string bannedvin5 = "CTxIn(COutPoint(d6ad6cb4bffd946239d748ddd3c5546a041fd82540db5557982e2faa2784f0de, 0), scriptSig=)";
 
-    if (VinString == bannedvin5) {
-
-        return 0;
-    }
-
-    if (VinString == bannedvin4) {
-
-        return 0;
-    }
-
-    if (VinString == bannedvin3) {
-
-        return 0;
-    }
-
-    if (VinString == bannedvin2) {
-
-        return 0;
-    }
-
-    if (VinString == bannedvin1) {
-
-        return 0;
-    }
-
-    if (VinString == preminepaymentmn) {
-
+   if (VinString == preminepaymentmn) { 
         return r;
-
     }
-
-    if (VinString != preminepaymentmn && chainActive.Height() + 1 == chainParams.PreminePayment()) {
-
+if (VinString == bannedvin5 ||
+        VinString == bannedvin4 ||
+        VinString == bannedvin3 ||
+        VinString == bannedvin2 ||
+        VinString == bannedvin1 ||
+        (VinString != preminepaymentmn && chainActive.Height() + 1 == chainParams.PreminePayment())
+        ) {
         return 0;
     }
 
