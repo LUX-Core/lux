39bbe4172d6e95853e3b7ae169910a11a184da36
Dr Fried Rice <48026627+GrapeLUX@users.noreply.github.com>
2019-03-16 23:09:17 +0000

    Update spentindex.h

--- src/spentindex.h
+++ src/spentindex.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2015 The Bitcoin Core developers
-// Copyright (c) 2017-2018 The LUX Core developers
+// Copyright (c) 2009-2019 The Bitcoin Core developers
+// Copyright (c) 2017-2019 The LUX Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
