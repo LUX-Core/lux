e5313deec3bec1e00e220b8f303b78ca12cc12ad
grapeLux2 <jajbroderick@gmail.com>
2019-10-23 19:57:57 +0100

    sameas previous

--- src/qt/coincontroldialog.h
+++ src/qt/coincontroldialog.h
@@ -118,7 +118,6 @@ private Q_SLOTS:
     void buttonSelectAllClicked();
     void HideInputAutoSelection();
     void ShowInputAutoSelection();
-    bool TX_size_limit(unsigned int size);
     void greater();
     void Less();
     void Equal();
