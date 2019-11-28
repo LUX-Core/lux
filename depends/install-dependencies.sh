1048b7bdffeec2502d552b47bc32b1f1a7de4589
Giacomo Milligan <giaki3003@gmail.com>
2019-11-28 21:38:25 +0100

    [depends] Remove duplicate packages, other small tweaks
    By Grape

--- depends/install-dependencies.sh
+++ depends/install-dependencies.sh
@@ -5,7 +5,7 @@ date
 #################################################################
 # Update Ubuntu and install prerequisites for running LUX   #
 #################################################################
-sudo add-apt-repository ppa:bitcoin/bitcoin
+sudo add-apt-repository -y ppa:bitcoin/bitcoin
 sudo apt-get update
 
 #################################################################
@@ -34,5 +34,4 @@ set -e
 #################################################################
 # Install all necessary packages for building LUX         #
 #################################################################
-sudo apt-get install -y libdb4.8-dev libdb4.8++-dev libzmq3-dev libminiupnpc-dev libcrypto++-dev libboost-all-dev build-essential libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libssl-dev ufw git software-properties-common libtool autotools-dev autoconf pkg-config libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libqrencode-dev automake g++-mingw-w64-x86-64 libevent-dev bsdmainutils curl git g++-mingw-w64-i686 mingw-w64-i686-dev g++-mingw-w64-x86-64
-
+sudo apt-get install -y libdb4.8-dev libdb4.8++-dev libzmq3-dev libminiupnpc-dev libcrypto++-dev libboost-all-dev build-essential libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libssl-dev ufw git software-properties-common libtool autotools-dev autoconf pkg-config libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libqrencode-dev automake g++-mingw-w64-x86-64 libevent-dev bsdmainutils curl git g++-mingw-w64-i686 mingw-w64-i686-dev
