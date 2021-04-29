ed5609ae8b24606566714fc9c931b55213db71ad
djm34 <djm34@aliceadsl.fr>
2021-04-29 16:50:27 +0300

    faster syncing with rx2 blocks

--- src/crypto/rx2.cpp
+++ src/crypto/rx2.cpp
@@ -22,7 +22,6 @@ uint256 GetRandomXSeed(const uint32_t& nHeight)
     uint32_t SeedStartingHeight = Params().GetConsensus().RX2SeedHeight;
     uint32_t SeedInterval = Params().GetConsensus().RX2SeedInterval;
     uint32_t SwitchKey = SeedStartingHeight % SeedInterval;
-
     if (current_key_block == uint256())
         current_key_block = chainActive.Genesis()->GetBlockHash();
 
@@ -33,14 +32,14 @@ uint256 GetRandomXSeed(const uint32_t& nHeight)
 
     if (nHeight > nHeight - remainer + SwitchKey) {
         
-        if ( nHeight  > first_check)
-            current_key_block = /*uint256("0xaf8367c7285833f5036ae884aadcc64a277b7e20bfb160cf57eaf7ece0e00c68size"); */ chainActive[first_check-SeedStartingHeight]->GetBlockHash();
+        if ( nHeight  > first_check) 
+            current_key_block = chainActive[first_check-SeedStartingHeight]->GetBlockHash();
+            
     } else {
            
-        if ( nHeight  > second_check) {
-            
-            current_key_block = /*uint256("0xaf8367c7285833f5036ae884aadcc64a277b7e20bfb160cf57eaf7ece0e00c68size"); */ chainActive[second_check-SeedStartingHeight]->GetBlockHash();
-        }
+        if ( nHeight  > second_check)             
+            current_key_block =  chainActive[second_check-SeedStartingHeight]->GetBlockHash();
+        
     }
     return current_key_block;
 }
@@ -75,7 +74,7 @@ void rx_slow_hash(const char* data, char* hash, int length, uint256 seedhash)
     }
     if (randomx_seed2!=seedhash) {
         randomx_seed2 = seedhash;
-//        randomx_reinit();
+
     randomx_destroy_vm(rx_vm);
     randomx_release_cache(cache);
 
@@ -84,7 +83,6 @@ void rx_slow_hash(const char* data, char* hash, int length, uint256 seedhash)
     rx_vm = randomx_create_vm(flags, cache, nullptr);
 
     }   
-
     randomx_calculate_hash(rx_vm, data, length, hash);
 
         LEAVE_CRITICAL_SECTION(cs_randomx) 
@@ -101,25 +99,26 @@ void rx_slow_hash2(const char* data, char* hash, int length, uint256 seedhash)
     static randomx_flags flags;
     static randomx_vm *rx_vm; // = nullptr;
     static randomx_cache *cache; // = nullptr;
+    static char old_data[288];
+    static char old_hash[64];
 
  
     if (!is_init) {
-
         randomx_seed2 = seedhash;
     if (!cache) {
         flags = randomx_get_flags();
         cache = randomx_alloc_cache(flags);
        
         randomx_init_cache(cache, randomx_seed2.GetHex().c_str(), randomx_seed2.GetHex().size());
+        is_init = true;
     }
  
     if (!rx_vm)
         rx_vm = randomx_create_vm(flags, cache, nullptr);
 
-    is_init = true;
+//    is_init = true;
     }
     if (randomx_seed2!=seedhash) {
-
         randomx_seed2 = seedhash;
 //        randomx_reinit();
     randomx_destroy_vm(rx_vm);
@@ -132,8 +131,14 @@ void rx_slow_hash2(const char* data, char* hash, int length, uint256 seedhash)
 
     }   
 
+    if (memcmp(data,old_data,144)==0) {            
+    memcpy(hash,old_hash,32);
+    }
+    else { 
     randomx_calculate_hash(rx_vm, data, length, hash);
-
+    memcpy(old_data,data,144);
+    memcpy(old_hash,hash,32);
+    }
         LEAVE_CRITICAL_SECTION(cs_randomx) 
 }
 
@@ -142,6 +147,7 @@ void rx_slow_hash2_old(const char* data, char* hash, int length, uint256 seedhas
 {
 
     bool is_init = false;
+    printf("rx_slow_hash2_old\n");
 //    static char randomx_seed[64]; //={0}; // this should be 64 and not 32
      uint256 randomx_seed2;
      
